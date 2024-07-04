35fec9e297ff6dd5b6b8678669d839192722753f
govindksgit <sharma.govindku@gmail.com>
2024-07-04 09:02:03 +0530

    linting fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import AllRoute from './AllRoute';
 import './App.css';
-import Card from './components/Card';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
